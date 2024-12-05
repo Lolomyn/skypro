f5f7d333f151e44866cdf82190e69dc5e8a36717
Savelev Maxim <vismanmark@yandex.ru>
2024-12-05 21:47:47 +0300

    debug is done

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-# CARD_NUMBER_STANDARD_LENGTH = 16
-# ACCOUNT_NUMBER_STANDARD_LENGTH = 20
+CARD_NUMBER_STANDARD_LENGTH = 16
+ACCOUNT_NUMBER_STANDARD_LENGTH = 20
 
 
-# def get_info() -> None:
-#     user_card_number = int(
-#         input("Добро пожаловать! Введите, пожалуйста, номер вашей карты (16 цифр), " "чтобы продолжить...\n")
-#     )
-#     if len(str(user_card_number)) == CARD_NUMBER_STANDARD_LENGTH:
-#         card_number_mask = get_mask_card_number(user_card_number)
-#         user_account_number = int(
-#             input(
-#                 f"Отлично! Ваша карта {card_number_mask} существует, можем "
-#                 f"продолжать.\nУкажите номер вашего счета (20 цифр), к которому привязана "
-#                 f"указанная карта...\n"
-#             )
-#         )
-#         if len(str(user_account_number)) == ACCOUNT_NUMBER_STANDARD_LENGTH:
-#             account_number_mask = get_mask_account(user_account_number)
-#             print(
-#                 f"Ваш счет {account_number_mask} принят.\n---\nВсе необходимые данные получены, спасибо, всего "
-#                 f"доброго!"
-#             )
-#         else:
-#             print("Указанные вами данные неверны, попробуйте снова!\n---\n")
-#             get_info()
-#     else:
-#         print("Указанные вами данные неверны, попробуйте снова!\n---\n")
-#         get_info()
+def get_info() -> None:
+    user_card_number = input(
+        "Добро пожаловать! Введите, пожалуйста, " "номер вашей карты (16 цифр), чтобы продолжить...\n"
+    )
+    card_number_mask = mask_account_card(user_card_number)
+    user_account_number = input(
+        f"Отлично! Ваша карта {card_number_mask} существует, можем "
+        f"продолжать.\nУкажите номер вашего счета (20 цифр), к которому привязана "
+        f"указанная карта...\n"
+    )
+    account_number_mask = mask_account_card(user_account_number)
+    print(f"Ваш счет {account_number_mask} принят.\n---\nВсе необходимые данные получены, спасибо, всего " f"доброго!")
 
 
 if __name__ == "__main__":
-    # get_info()
-    pass
+    get_info()
--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,21 @@
-import masks
+from .masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_data: str) -> str:
-	"""Функция, которая принимает номер карты или счета и возвращает его замаскированным"""
-	return masks.get_mask_account(user_data.split()[-1]) if len(user_data.split()[-1]) == 20 \
-		else masks.get_mask_card_number(user_data.split()[-1])
+    """Функция, которая принимает номер карты или счета и возвращает его замаскированным"""
+    return (
+        get_mask_account(user_data.split()[-1])
+        if len(user_data.split()[-1]) == 20
+        else get_mask_card_number(user_data.split()[-1])
+    )
 
 
 # print(mask_account_card("Счет 73654108430135874305"))
 
 
 def get_date(cur_date: str) -> str:
-	"""Функция, которая принимает дату в формате "2024-03-11T02:26:18.671407" возвращает строку в формате ДД.ММ.ГГГГ"""
-	return f"{cur_date[:10].split('-')[2]}.{cur_date[:10].split('-')[1]}.{cur_date[:10].split('-')[0]}"
+    """Функция, которая принимает дату в формате "2024-03-11T02:26:18.671407" возвращает строку в формате ДД.ММ.ГГГГ"""
+    return f"{cur_date[:10].split('-')[2]}.{cur_date[:10].split('-')[1]}.{cur_date[:10].split('-')[0]}"
 
 
 # print(get_date("2024-03-11T02:26:18.671407"))
