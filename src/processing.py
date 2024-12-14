edb0a19ab3e75bfc370189d25f1bd3eccec489ea
Savelev Maxim <vismanmark@yandex.ru>
2024-12-14 23:13:45 +0300

    done func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,8 @@
-def filter_by_state(list_of_dict: list, state_key_value='EXECUTED') -> list:
+def filter_by_state(list_of_dicts: list, state_key_value: str = "EXECUTED") -> list:
 	"""Функция, которая возвращает список словарей, значение ключа state которых соответствует указанному"""
-	return [i for i in list_of_dict if i['state'] == state_key_value]
+	return [i for i in list_of_dicts if i["state"] == state_key_value]
 
+
+def sort_by_date(list_of_dicts: list, sort_order: bool = True) -> list:
+	"""Функция, которая возвращает отсортированный список словарей по дате (в порядке убывания или возрастания)"""
+	return sorted(list_of_dicts, key=lambda x: x["date"], reverse=sort_order)
