b55bacc3ac5ad1c30241d08ab57255b15b2175ff
Savelev Maxim <vismanmark@yandex.ru>
2024-12-25 23:07:45 +0300

    add generators.py pattern

---
+++ src/generators.py
@@ -0,0 +1,15 @@
+def filter_by_currency(transactions, currency) -> list:
+	"""
+	Создается итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+
+	:param transactions:
+	:param currency:
+	:return:
+	"""
+	pass
+
+
+transaction_descriptions = ()
+
+card_number_generator = ()
+
