0a15cbb1e5c70c137e2370a322808e4da655f0fd
Savelev Maxim <vismanmark@yandex.ru>
2025-01-02 21:48:47 +0300

    fixes

--- src/decorators.py
+++ src/decorators.py
@@ -4,24 +4,28 @@ from typing import Any, Callable
 
 
 def log(filename: str) -> Callable:
+    """Декоратор, выводящий логи в консоль или файл о корректном или некорректном исполнении функции"""
     def wrapper(func: Any) -> Any:
         @wraps(func)
         def inner(*args: Any, **kwargs: Any) -> Any:
+            res = ''
             try:
-                func(*args, **kwargs)
+                res = func(*args, **kwargs)
             except Exception as e:
                 if os.path.exists(filename):
                     with open(filename, "a") as file:
                         file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
                 else:
                     print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                return res
+
             else:
                 if os.path.exists(filename):
                     with open(filename, "a") as file:
                         file.write(f"{func.__name__} ok\n")
                 else:
                     print(f"{func.__name__} ok")
-
+                return res
         return inner
 
     return wrapper
