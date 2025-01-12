d7e2ebe50060115655e9dd7cf8aed2ec9a88dcf8
Savelev Maxim <vismanmark@yandex.ru>
2025-01-12 23:17:38 +0300

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,15 @@
-import json
 import codecs
+import json
+import os
 
 
-def get_list_of_operations(path_to_file: str) -> list:
-	with codecs.open(path_to_file, 'r', 'utf_8_sig') as json_file:
-		operations = json.load(json_file)
+def get_list_of_operations(file_path: str) -> list:
+    if os.path.isfile(file_path) and os.path.getsize(file_path) != 0:
+        with codecs.open(file_path, "r", "utf_8_sig") as json_file:
+            operations = json.load(json_file)
+        if isinstance(operations, list):
+            return operations
+    return []
 
-	return operations
 
-
-get_list_of_operations('data/operations.json')
+print(get_list_of_operations("data/operations.json"))
