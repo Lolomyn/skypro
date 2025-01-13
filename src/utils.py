43f6dfcfb516996b3c829b8b413696638c23553f
Savelev Maxim <vismanmark@yandex.ru>
2025-01-13 22:14:51 +0300

    do func get_amount

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import os
 
 
 def get_list_of_operations(file_path: str) -> list:
+    """Возвращает список словарей с данными о финансовых транзакциях"""
     if os.path.isfile(file_path) and os.path.getsize(file_path) != 0:
         with codecs.open(file_path, "r", "utf_8_sig") as json_file:
             operations = json.load(json_file)
